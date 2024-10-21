b526b2fcd9c4f7e9e4733b30f3385462a2100056
88885 <V.Gucu@rendez-vous.ru>
2024-10-21 12:01:18 +0300

    Спринт 12. Тестовое изменение.

--- app/src/main/java/com/example/playlistmaker/search/ITunesApi.kt
+++ app/src/main/java/com/example/playlistmaker/search/ITunesApi.kt
@@ -9,5 +9,7 @@ import retrofit2.http.Query
 
 interface ITunesApi {
     @GET("/search?entity=song")
-    fun findSong(@Query("term") text:String): Call<ITunesResponse>
+    fun findSong(
+        @Query("term") text:String
+    ): Call<ITunesResponse> // аннотация @Query позволяет фильтровать запрос по параметру
 }
