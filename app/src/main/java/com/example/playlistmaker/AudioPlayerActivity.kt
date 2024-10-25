927878734f22490d4921438cd011e7e5d511965f
88885 <V.Gucu@rendez-vous.ru>
2024-10-25 15:43:20 +0300

    Спринт 13. Домашнее задание. Частично. vol.1

---
+++ app/src/main/java/com/example/playlistmaker/AudioPlayerActivity.kt
@@ -0,0 +1,73 @@
+package com.example.playlistmaker
+
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.transition.Visibility
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.example.playlistmaker.search.AudioPlayerCurrentTrack
+import com.example.playlistmaker.search.Track
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class AudioPlayerActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_audioplayer)
+
+        findViewById<TextView>(R.id.audioplayerBack).setOnClickListener{ finish() } // возвращение на главный экран
+
+        val currentTrack=intent.getSerializableExtra(AudioPlayerCurrentTrack.CURRENT_TRACK) as Track
+        Log.d("currentTrack", "${currentTrack.trackName}, ${currentTrack.collectionName} ${currentTrack.collectionName?.length}")
+        createAlbumСover(currentTrack)
+    }
+
+
+
+    private fun createAlbumСover(track: Track) {
+
+        val trackLogo: ImageView = this.findViewById(R.id.trackLogo)
+
+        val trackDurationValue: TextView = this.findViewById(R.id.trackDuration2)
+        trackDurationValue.text = SimpleDateFormat("mm:ss", Locale.getDefault()).format(track.trackTimeMillis.toInt())
+
+        val artistName: TextView = this.findViewById(R.id.trackArtist)
+        artistName.text = track.artistName
+
+        val trackName: TextView = this.findViewById(R.id.trackName)
+        trackName.text = track.artistName
+
+        val trackAlbum: TextView = this.findViewById(R.id.trackAlbum)
+        val trackAlbumValue: TextView = this.findViewById(R.id.trackAlbum2)
+        if(track.collectionName.isNullOrEmpty()) {
+            trackAlbum.visibility = View.GONE
+        }
+        else trackAlbumValue.text = track.collectionName
+
+        val trackYearValue: TextView = this.findViewById(R.id.trackYear2)
+        trackYearValue.text = SimpleDateFormat("yyyy", Locale.getDefault()).format(track.releaseDate!!)
+
+        val trackGenreValue: TextView = this.findViewById(R.id.trackGenre2)
+        trackGenreValue.text = track.primaryGenreName
+
+        val trackCountryValue: TextView = this.findViewById(R.id.trackCountry2)
+        trackCountryValue.text = track.country
+
+
+        Glide.with(this@AudioPlayerActivity)
+            .load(track.artworkUrl100.replaceAfterLast('/', "512x512bb.jpg"))
+            .fitCenter()
+            .placeholder(R.drawable.ic_placeholder_audioplayer)
+            .error(R.drawable.ic_placeholder_audioplayer)
+            .centerCrop()
+            .transform(RoundedCorners(10))
+            .into(trackLogo)
+    }
+
+}
--- app/src/main/java/com/example/playlistmaker/search/Track.kt
+++ app/src/main/java/com/example/playlistmaker/search/Track.kt
@@ -1,10 +1,40 @@
 package com.example.playlistmaker.search;
 
-data class Track(
-        val trackId: String, // id трека
-        val trackName: String, // Название композиции
-        val artistName: String, // Имя исполнителя
-        val trackTimeMillis: String, // Продолжительность трека
-        val artworkUrl100: String // Ссылка на изображение обложки
-) {
+import java.io.Serializable
+import java.util.Date
+
+class Track: Serializable {
+
+        var trackId: String = "" // id трека
+        var trackName: String = "" // Название композиции
+        var artistName: String = "" // Имя исполнителя
+        var trackTimeMillis: String = "" // Продолжительность трека
+        var artworkUrl100: String = "" // Ссылка на изображение обложки
+        var collectionName: String? = "" // Название альбома
+        var releaseDate: Date? = null // Год релиза трека
+        var primaryGenreName: String = "" // Жанр трека
+        var country: String = "" // Страна исполнителя
+
+        constructor(
+                trackId: String,
+                trackName: String,
+                artistName: String,
+                trackTimeMillis: String,
+                artworkUrl100: String,
+                collectionName: String?,
+                releaseDate: Date?,
+                primaryGenreName: String,
+                country: String
+        ){
+                this.trackId = trackId
+                this.trackName = trackName
+                this.artistName = artistName
+                this.trackTimeMillis = trackTimeMillis
+                this.artworkUrl100 = artworkUrl100
+                this.collectionName= collectionName
+                this.releaseDate= releaseDate
+                this.primaryGenreName= primaryGenreName
+                this.country= country
+        }
+        constructor()
 }
